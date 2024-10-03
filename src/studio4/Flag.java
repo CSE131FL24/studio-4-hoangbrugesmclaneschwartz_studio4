026bb8403d8741617d63b71ba661532778d0614b
vince <vince@localhost>
2024-10-03 12:41:50 -0500

    Finsihed studio

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,35 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		int red = 255;
+		int green = 255;
+		int blue = 0;
+		StdDraw.setPenColor(red, green, blue);
 		
+		double x = .5;
+		double y = .7;
+		double major = .15;
+		double minor = .15;
+		StdDraw.filledEllipse(x, y, major, minor);
+		
+		x = .7;
+		y = .5;
+		StdDraw.filledEllipse(x, y, major, minor);
+		
+		x = .5;
+		y = .3;
+		StdDraw.filledEllipse(x, y, major, minor);
+		
+		x = .3;
+		y = .5;
+		StdDraw.filledEllipse(x, y, major, minor);
+		
+		x = .5;
+		y = .5;
+		double width = .2;
+		double height = .2;
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledRectangle(x, y, width, height);
+				
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,51 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		boolean fill = false;
+		String shape = in.next();
+		if (shape.equals("rectangle")) {
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			StdDraw.setPenColor(red, green, blue);
+			
+			if (in.nextBoolean()) {
+				fill = true;
+			}
+			
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double width = in.nextDouble();
+			double height = in.nextDouble();
+			
+			if (fill == true) {
+				StdDraw.filledRectangle(x, y, width, height);
+			}
+			else {
+				StdDraw.rectangle(x, y, width, height);
+			}
+		}
+		//ellipses
+		if (shape.equals("ellipse")) {
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			StdDraw.setPenColor(red, green, blue);
+			
+			if (in.nextBoolean()) {
+				fill = true;
+			}
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double major = in.nextDouble();
+			double minor = in.nextDouble();
+			
+			if (fill == true) {
+				StdDraw.filledEllipse(x, y, major, minor);
+			}
+			else {
+				StdDraw.ellipse(x, y, major, minor);
+			}
+		}
 	}
 }
